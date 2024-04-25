704cb4fbdc9de647f0be6d1a8c599f3e381446ae
vishaldhakal <vishaldhakal96@gmail.com>
2024-04-25 13:57:22 +0545

    fV

--- yachu/settings.py
+++ yachu/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'yachu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
-""" DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "hikingbees",
+        "NAME": "yachu",
         "USER": "vishal",
         "PASSWORD": "DatabaseUserPassword",
         "HOST": "localhost",
         "PORT": "",
     }
-} """
+}
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = None
 
