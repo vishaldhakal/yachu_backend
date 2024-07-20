75b639acd3a1a77704765cc295ab6be902fc2536
vishaldhakal <vishaldhakal96@gmail.com>
2024-07-20 17:13:13 +0545

    Dfv

--- cim/urls.py
+++ cim/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('franchise/',views.StallBookingListCreateView.as_view(),name='stall-list-create'),
-   path('franchise/<int:pk>/',views.StallBookingRetrieveUpdateDestroyView.as_view(),name='stall-retrieve-update-destroy'),
+   path('stall/',views.StallBookingListCreateView.as_view(),name='stall-list-create'),
+   path('stall/<int:pk>/',views.StallBookingRetrieveUpdateDestroyView.as_view(),name='stall-retrieve-update-destroy'),
 ]
