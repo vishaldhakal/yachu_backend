5f0010ab9b9063ebee2b723ac172cb2c3454564c
vishaldhakal <vishaldhakal96@gmail.com>
2024-07-21 08:19:42 +0545

    dfv

--- cim/views.py
+++ cim/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import StallBooking
-from .serializers import StallBookingSerializer
+from .serializers import StallBookingSerializer,StallBookingSmallSerializer
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
@@ -47,9 +47,14 @@ def reject_stall(request, pk):
 
 @api_view(['GET'])
 def get_booked_stalls(request):
-   booked_stalls = StallBooking.objects.filter(status='Approved')
-   pending_booked = StallBooking.objects.filter(status='Pending')
+   stall_type = request.GET.get('stall_type', None)
+   if stall_type is not None:
+      booked_stalls = StallBooking.objects.filter(status='Approved', stall_type=stall_type)
+      pending_booked = StallBooking.objects.filter(status='Pending', stall_type=stall_type)
+   else:
+      booked_stalls = StallBooking.objects.filter(status='Approved')
+      pending_booked = StallBooking.objects.filter(status='Pending')
 
-   serializer = StallBookingSerializer(booked_stalls, many=True)
-   serializer2 = StallBookingSerializer(pending_booked, many=True) 
+   serializer = StallBookingSmallSerializer(booked_stalls, many=True)
+   serializer2 = StallBookingSmallSerializer(pending_booked, many=True) 
    return Response({"booked": serializer.data, "pending": serializer2.data})
