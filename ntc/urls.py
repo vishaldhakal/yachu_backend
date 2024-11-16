72ece9bc7e8ff3ddb87b1ae333f0a7f6df57494d
vishaldhakal <vishaldhakal96@gmail.com>
2024-11-16 14:49:17 +0545

    DSV

--- ntc/urls.py
+++ ntc/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import GlobalSearchView
 
 urlpatterns = [
-    path('/search/', GlobalSearchView.as_view(), name='global-search'),
+    path('search/', GlobalSearchView.as_view(), name='global-search'),
 ]
