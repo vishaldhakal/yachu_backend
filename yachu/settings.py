440d85d976138e7a0029e0c0777e43c4a5039982
vishaldhakal <vishaldhakal96@gmail.com>
2024-11-18 15:25:58 +0545

    SD

--- yachu/settings.py
+++ yachu/settings.py
@@ -374,7 +374,7 @@ UNFOLD = {
     },
 }
 
-# DRF Settings
+""" # DRF Settings
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
@@ -382,3 +382,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+ """
