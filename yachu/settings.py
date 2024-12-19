3aab7e613aba7fc4f2ee1ec91aa03654f599013f
vishaldhakal <vishaldhakal96@gmail.com>
2024-12-19 09:13:08 +0545

    Dfv

--- yachu/settings.py
+++ yachu/settings.py
@@ -90,13 +90,13 @@ WSGI_APPLICATION = 'yachu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-"""DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} 
-"""
+}  """
+
 
 DATABASES = {
     "default": {
