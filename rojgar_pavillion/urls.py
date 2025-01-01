771f1a8f8f3390ca93e9225fb06256e40e2a371c
vishaldhakal <vishaldhakal96@gmail.com>
2025-01-01 20:14:08 +0545

    DFV'

--- rojgar_pavillion/urls.py
+++ rojgar_pavillion/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import RegistrationView,AvailableSessionsView,TimeSlotByDateView,RegistrationDetailView
+from .views import (
+    RegistrationView,
+    AvailableSessionsView,
+    TimeSlotByDateView,
+    RegistrationDetailView,
+)
 
 urlpatterns = [
-    path('registrations/', RegistrationView.as_view(), name='registration-list'),
-    path('registrations/available-sessions/', AvailableSessionsView.as_view(), name='available-sessions'),
-    path('registrations/<int:pk>/', RegistrationDetailView.as_view(), name='registration-detail'),
-    path('timeslots/', TimeSlotByDateView.as_view(), name='timeslot-by-date'),
+    path("registrations/", RegistrationView.as_view(), name="registration-list"),
+    path(
+        "registrations/available-sessions/",
+        AvailableSessionsView.as_view(),
+        name="available-sessions",
+    ),
+    path(
+        "registrations/<int:pk>/",
+        RegistrationDetailView.as_view(),
+        name="registration-detail",
+    ),
+    path("timeslots/", TimeSlotByDateView.as_view(), name="timeslot-by-date"),
 ]
