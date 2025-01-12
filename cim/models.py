d4bbf757275b5827177529ddd78af8ef2ef0e221
Ratish149 <ratish.shakya149@gmail.com>
2025-01-12 14:50:23 +0545

    dfdf

--- cim/models.py
+++ cim/models.py
@@ -91,7 +91,7 @@ class ThematicSession(models.Model):
     date = models.DateField()
     start_time = models.TimeField()
     end_time=models.TimeField(blank=True, null=True)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -103,7 +103,7 @@ class SubSession(models.Model):
         related_name="sub_sessions"
     )
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} - {self.thematic_session.title}"
@@ -126,7 +126,7 @@ class Panelist(models.Model):
     profile_image = models.ImageField(upload_to="panelists/", blank=True, null=True)
     company = models.CharField(max_length=255, blank=True, null=True)
     location = models.CharField(max_length=255, blank=True, null=True)
-    biodata = models.TextField()
+    biodata = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.role}: {self.name}"
