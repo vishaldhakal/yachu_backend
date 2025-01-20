faf83a370b865904c761f54df7672809ba9b1556
Ratish149 <ratish.shakya149@gmail.com>
2025-01-20 17:39:20 +0545

    dfdd

--- rojgar_pavillion/admin.py
+++ rojgar_pavillion/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from unfold.admin import ModelAdmin,TabularInline
+from unfold.admin import ModelAdmin, TabularInline
 from .models import Topic, TimeSlot, Registration
+
+class RegistrationAdmin(ModelAdmin):
+    list_display = ('time_slot', 'registration_type', 'total_participants', 'total_price', 'created_at')
+    list_filter = ('registration_type', 'created_at', 'time_slot')
 
 admin.site.register(Topic, ModelAdmin)
 admin.site.register(TimeSlot, ModelAdmin)
-admin.site.register(Registration, ModelAdmin)
+admin.site.register(Registration, RegistrationAdmin)
