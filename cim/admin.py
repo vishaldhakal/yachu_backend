df2783f351e036df38cb6448eec0a0c963b11a40
Ratish149 <ratish.shakya149@gmail.com>
2025-01-22 16:23:25 +0545

    sdsd

--- cim/admin.py
+++ cim/admin.py
@@ -12,14 +12,22 @@ class TinyMce(ModelAdmin):
     }
 
 class ThematicSessionAdmin(ModelAdmin):
+    
     list_display = ('title', 'date', 'start_time', 'end_time')
     search_fields = ('title',)
     list_filter = ('date',)
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+    }
 
 class SubSessionAdmin(ModelAdmin):
+    
     list_display = ('title', 'thematic_session')
     search_fields = ('title', 'thematic_session__title')
     list_filter = ('thematic_session',)
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+    }
 
 admin.site.register(StallBooking,ModelAdmin)
 admin.site.register(SponsorBooking,ModelAdmin)
