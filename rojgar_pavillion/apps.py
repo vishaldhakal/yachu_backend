7461c4524385a222327d7b70516550e8b4e3aa55
Ratish149 <ratish.shakya149@gmail.com>
2025-03-20 13:58:49 +0545

    dfd

--- rojgar_pavillion/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RojgarPavillionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rojgar_pavillion'
-
-    def ready(self):
-        import rojgar_pavillion.signals
--- yachu/settings.py
+++ yachu/settings.py
@@ -49,8 +49,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'solo',
     'corsheaders',
-    'cim',
-    'rojgar_pavillion'
+
 ]
 
 MIDDLEWARE = [
