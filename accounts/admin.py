77abb883310f63e498d1f383c3a511a68c989934
Ratish149 <ratish.shakya149@gmail.com>
2025-04-09 11:18:21 +0545

    dfd

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import CustomUser
+from unfold.admin import ModelAdmin
 # Register your models here.
 
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, ModelAdmin)
