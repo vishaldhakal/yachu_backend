281b94cd87c7b739908b8085d745b22c4f59252c
Ratish149 <ratish.shakya149@gmail.com>
2025-05-14 14:12:03 +0545

    dfd

--- finance_management/models.py
+++ finance_management/models.py
@@ -15,6 +15,7 @@ class FinanceRecord(models.Model):
         max_length=10, choices=TRANSACTION_TYPE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     remarks = models.TextField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
