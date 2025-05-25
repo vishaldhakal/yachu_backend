f9a64230c5c538e58cc7fe6f38dcbc89b32079d3
Ratish149 <ratish.shakya149@gmail.com>
2025-05-25 13:25:32 +0545

    dfd

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ class Organization(models.Model):
     name = models.CharField(max_length=100)
     person_in_charge = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=10, null=True, blank=True)
+    opening_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     address = models.CharField(max_length=255, null=True, blank=True)
     remarks = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
