9dd51b71eb55ed2e9da86c6a79c3c5bb61791b41
Ratish149 <ratish.shakya149@gmail.com>
2025-06-03 12:57:59 +0545

    dfd

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, OrganizationListCreateView, OrganizationDetailView, DepartmentListCreateView
+from .views import RegisterView, LoginView, OrganizationListCreateView, OrganizationDetailView, DepartmentListCreateView, DepartmentDetailView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
