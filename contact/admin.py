84e3ec4be4c547c47b59e67b2baf391d4132cea2
Ratish149 <ratish.shakya149@gmail.com>
2025-08-19 11:16:24 +0545

    sds

--- contact/admin.py
+++ contact/admin.py
@@ -3,10 +3,12 @@ from .models import Contact
 from unfold.admin import ModelAdmin
 # Register your models here.
 
+
 class ContactAdmin(ModelAdmin):
     list_display = ('full_name', 'email', 'phone', 'message', 'created_at')
-    list_filter = ('created_at')
-    search_fields = ('full_name','phone')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('full_name', 'phone')
     ordering = ('-created_at',)
+
 
 admin.site.register(Contact, ContactAdmin)
