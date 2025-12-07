d788c621259834ca9a41b2af9c78e7e2ee27dc89
Ratish149 <ratish.shakya149@gmail.com>
2025-12-07 14:23:48 +0545

    dfd

--- ai_data/models.py
+++ ai_data/models.py
@@ -4,6 +4,11 @@ from django.db import models
 # Create your models here.
 class AIData(models.Model):
     ai_json_data = models.JSONField()
+    extracted_json_data = models.JSONField(null=True, blank=True)
+    conversation_json_data = models.JSONField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.id)
