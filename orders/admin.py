66a62d9ad3f2a9c413874ca16151f2f257b13894
Ratish149 <ratish.shakya149@gmail.com>
2025-12-22 11:01:39 +0545

    added instant order to admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin, TabularInline
-from .models import Seller, OrderProduct, Order, Commission, Tracking
+
+from .models import Commission, InstantOrder, Order, OrderProduct, Seller, Tracking
 
 
 @admin.register(Seller)
 class SellerAdmin(ModelAdmin):
-    list_display = ('user', 'phone_number', 'commission_rate')
-    search_fields = ('user__username', 'user__email', 'phone_number')
+    list_display = ("user", "phone_number", "commission_rate")
+    search_fields = ("user__username", "user__email", "phone_number")
 
 
 class OrderProductInline(TabularInline):
@@ -17,10 +18,15 @@ class OrderProductInline(TabularInline):
 @admin.register(Order)
 class OrderAdmin(ModelAdmin):
     inlines = [OrderProductInline]
-    list_display = ('full_name', 'phone_number',
-                    'total_amount', 'order_status', 'created_at')
-    list_filter = ('order_status',)
-    search_fields = ('full_name', 'phone_number')
+    list_display = (
+        "full_name",
+        "phone_number",
+        "total_amount",
+        "order_status",
+        "created_at",
+    )
+    list_filter = ("order_status",)
+    search_fields = ("full_name", "phone_number")
 
     def save_model(self, request, obj, form, change):
         is_new = obj.pk is None
@@ -31,17 +37,30 @@ class OrderAdmin(ModelAdmin):
 
 @admin.register(Commission)
 class CommissionAdmin(ModelAdmin):
-    list_display = ('seller', 'order', 'amount', 'paid', 'created_at')
-    list_filter = ('paid', 'seller')
-    search_fields = ('seller__user__username', 'order__full_name')
+    list_display = ("seller", "order", "amount", "paid", "created_at")
+    list_filter = ("paid", "seller")
+    search_fields = ("seller__user__username", "order__full_name")
 
 
 @admin.register(Tracking)
 class TrackingAdmin(ModelAdmin):
-    list_display = ('details', 'created_at', 'updated_at')
-    search_fields = ('details',)
-    list_filter = ('created_at', 'updated_at')
-    ordering = ('-created_at',)
-    readonly_fields = ('created_at', 'updated_at')
+    list_display = ("details", "created_at", "updated_at")
+    search_fields = ("details",)
+    list_filter = ("created_at", "updated_at")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at", "updated_at")
     list_per_page = 10
     list_max_show_all = 100
+
+
+@admin.register(InstantOrder)
+class InstantOrderAdmin(ModelAdmin):
+    list_display = (
+        "name",
+        "phone_number",
+        "address",
+        "quantity",
+        "created_at",
+    )
+    list_filter = ("quantity",)
+    search_fields = ("name", "phone_number")
